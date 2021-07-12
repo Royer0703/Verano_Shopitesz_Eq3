17a13aaefe20121420f0fbd709ce8550efc338d0
Royer0703 <61436502+Royer0703@users.noreply.github.com>
2021-07-12 12:38:11 -0500

    Roger

---
+++ ShopiteszEq3/static/js/jslogin.js
@@ -0,0 +1,7 @@
+function validarcorreo(correo) {
+    var expReg= /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
+    var esValido= expReg.test(correo);
+    if (esValido!=true) {
+        alert('Correo invalido');
+    }
+}
