8291164e838d9c6e2aa90a5efa698066ffd9b86b
napo315 <61436344+napo315@users.noreply.github.com>
2021-07-19 11:57:38 -0500

    nap

--- ShopiteszEq3/BD Shopitesz.sql
+++ ShopiteszEq3/BD Shopitesz.sql
@@ -1,7 +1,8 @@
 /*CREACION DE LA BD*/
 create database shopitesz;
+drop database shopitesz;
 use shopitesz;
-
+---------------------------------/*CATEGORIA*/
 create table Categorias(
 	idCategoria int auto_increment not null,
     nombre varchar(60) not null,
@@ -13,6 +14,8 @@ create table Categorias(
     constraint chk_estatus check (estatus in('Activa', 'Inactiva'))
 );
 
+
+---------------------------------/*PRODUCTOS*/
 create table Productos(
 	idProducto int auto_increment not null,
     idCategoria int not null,
@@ -28,6 +31,7 @@ create table Productos(
     foreign key(idCategoria) references categorias(idCategoria)
 );
 
+-------------------------/*USUARIOS*/
 create table Usuarios(
 	idUsuario int auto_increment not null,
     nombreCompleto varchar(60) not null,
@@ -42,9 +46,81 @@ create table Usuarios(
     constraint pk_usuarios primary key(idUsuario)
 );
 
+
+----------------------/*CARRITO*/
+create table Carrito(
+	idCarrito int auto_increment not null,
+    idUsuario int not null,
+    idProducto int not null,
+    fecha date not null,
+	cantidad int not null,
+    estatus varchar(10) not null,
+    
+    constraint pk_carrito primary key(idCarrito),
+    foreign key(idUsuario) references usuarios(idUsuario),
+    foreign key(idProducto) references producto(idProducto)
+);
+
+----------------------/*TARJETA*/
+create table Tarjetas(
+	idTarjeta int auto_increment not null,
+    idUsuario int not null,
+    noTarjeta varchar(16) not null,
+	saldo float not null,
+    Banco varchar(50) not null,
+    estatus varchar(10) not null,
+    
+    constraint pk_tarjetas primary key(idTarjeta),
+    foreign key(idUsuario) references usuarios(idUsuario)
+);
+
+----------------------/*Pedidos*/
+create table Pedidos(
+	idPedido int auto_increment not null,
+    idComprador int not null,
+    idVendedor int not null,
+    idTarjeta int not null,
+    fechaRegistro date not null,
+    fechaAtencion date not null,
+    fechaRecepcion date not null,
+    fechaCierre date not null,
+    total float not null,
+    estatus varchar(10) not null,
+    
+    constraint pk_pedidos primary key(idPedido),
+    foreign key(idComprador) references comprador(idComprador),
+    foreign key(idVendedor) references vendedor(idVendedor),
+    foreign key(idTarjeta) references tarjeta(idTarjeta)
+);
+
+----------------------/*DetallesPedidos*/
+create table DetallePedidos(
+	idDetalle int auto_increment not null,
+    idPedido int not null,
+    idProducto int not null,
+	precio float not null,
+    cantidadPedida int not null,
+    cantidadEnviada int not null,
+    cantidadAceptada int not null,
+    cantidadRechazada int not null,
+    subtotal float not null,
+    estatus varchar(10) not null,
+	comentario varchar(200) not null,
+    
+    constraint pk_detalle primary key(idDetalle),
+    foreign key(idPedido) references pedido(idPedido),
+    foreign key(idProducto) references productor(idProducto)
+);
+
+drop table Categorias;
+insert into categorias(nombre,estatus) values('Equipo de computo','Activa');
+select * from Categorias;
+
+/*crear usuarios*/
+create user user_shopitesz identified by 'Shopit3sz.123';
+
 drop table Usuarios;
 select * from Productos;
 
 grant select,insert,update,delete on shopitesz.categorias to user_shopitesz;
 grant select,insert,update,delete on shopitesz.Usuarios to user_shopitesz;
-grant select,insert,update,delete on shopitesz.Productos to user_shopitesz;
